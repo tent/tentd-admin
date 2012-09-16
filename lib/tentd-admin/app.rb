1cd20f60dc2a2f8c4ff74c14de34ab4538e0ea46
Jonathan Rudenberg <jonathan@titanous.com>
2012-09-16 13:32:52 -0400

    Remove hardcoded client

--- lib/tentd-admin/app.rb
+++ lib/tentd-admin/app.rb
@@ -13,61 +13,12 @@ module TentD
   class Admin < Sinatra::Base
     require 'tentd-admin/sprockets/environment'
 
-    AdminConfig = Struct.new(:app, :app_authorization).new(nil, nil)
-
-    enable :sessions
-
     configure :development do |config|
       require 'sinatra/reloader'
       register Sinatra::Reloader
       config.also_reload "*.rb"
 
       config.method_override = true
-    end
-
-    configure do
-      # Setup Database
-      DataMapper.setup(:default, ENV['DATABASE_URL'])
-      DataMapper.auto_upgrade!
-
-      # Init App/AppAuthorization
-      ::TentD::Model::User.current ||= ::TentD::Model::User.first_or_create
-      mac_key_id = "00000000"
-      unless tent_app = ::TentD::Model::App.first(:mac_key_id => mac_key_id)
-        tent_app = ::TentD::Model::App.create(
-          :name => "Tent Admin",
-          :description => "Default Tent Admin App",
-          :url => 'https://tent.io',
-          :mac_key_id => mac_key_id,
-          :redirect_uris => %w{ http://localhost:5000/admin?foo=bar },
-          :scopes => {
-            "read_posts" => "Show posts feed",
-            "write_posts" => "Publish posts",
-            "read_profile" => "Read your profile"
-          }
-        )
-
-        tent_app.authorizations.create(
-          :scopes => %w{ read_posts write_posts import_posts read_profile write_profile read_followers write_followers read_followings write_followings read_groups write_groups read_permissions write_permissions read_apps write_apps follow_ui read_secrets write_secrets },
-          :profile_info_types => ['all'],
-          :post_types => ['all']
-        )
-      end
-
-      # Set TENT_ENTITY ENV
-      if !ENV['TENT_ENTITY']
-        core_type = TentD::TentType.new(TentD::Model::ProfileInfo::TENT_PROFILE_TYPE_URI)
-        if info = ::TentD::Model::ProfileInfo.first(:type_base => core_type.base)
-          ENV['TENT_ENTITY'] = info.content['entity']
-        end
-      end
-
-      AdminConfig.app = tent_app
-      AdminConfig.app_authorization = tent_app.authorizations.first
-    end
-
-    use Rack::Auth::Basic, "Admin Area" do |username, password|
-      username == ENV['ADMIN_USERNAME'] && password == ENV['ADMIN_PASSWORD']
     end
 
     use Rack::Csrf
@@ -132,7 +83,7 @@ module TentD
       end
 
       def tent_client
-        ::TentClient.new(server_url, AdminConfig.app_authorization.auth_details)
+        env['tent.client']
       end
     end
 
@@ -142,51 +93,12 @@ module TentD
       assets.call(new_env)
     end
 
-    before do
-      if ::TentD::Model::ProfileInfo.count == 0 && env['PATH_INFO'] != '/setup' && env['PATH_INFO'] !~ /^\/assets/
-        redirect full_path('setup')
-        return
-      end
-    end
-
     get '/' do
       slim :dashboard
     end
 
-    get '/setup' do
-      if ::TentD::Model::ProfileInfo.count > 0
-        redirect full_path('')
-        return
-      end
-
-      AdminConfig.app_authorization.update(:follow_url => "#{server_url}#{full_path('/followings')}")
-
-      @core_profile_info = Hashie::Mash.new(
-        :entity => ENV['TENT_ENTITY'] || server_url,
-        :servers => [server_url]
-      )
-
-      slim :setup
-    end
-
-    post '/setup' do
-      core_profile_info = {
-        :entity => params[:entity],
-        :servers => [params[:servers]],
-        :licenses => [],
-        :public => true
-      }
-
-      ENV['TENT_ENTITY'] = params[:entity]
-
-      client = tent_client
-      client.profile.update(TentD::Model::ProfileInfo::TENT_PROFILE_TYPE_URI, core_profile_info)
-      redirect full_path('')
-    end
-
     get '/profile' do
-      client = tent_client
-      @profile = client.profile.get.body
+      @profile = tent_client.profile.get.body
       @profile['https://tent.io/types/info/basic/v0.1.0'] ||= {
         'public' => true,
         'name' => 'Name to be displayed publicly',
@@ -200,18 +112,16 @@ module TentD
     end
 
     put '/profile' do
-      client = tent_client
       params.each_pair do |key, val|
         next unless key =~ %r{tent.io/types/info}
-        client.profile.update(key, val)
+        tent_client.profile.update(key, val)
       end
 
       redirect full_path('/profile')
     end
 
     get '/followings' do
-      client = tent_client
-      @followings = client.following.list.body
+      @followings = tent_client.following.list.body
       @followings.map! { |f| Hashie::Mash.new(f) }
       @entity = URI.decode(params[:entity]) if params[:entity]
       slim :followings
@@ -219,8 +129,7 @@ module TentD
 
     post '/followings' do
       begin
-        client = tent_client
-        client.following.create(params[:entity])
+        tent_client.following.create(params[:entity])
         redirect full_path('/followings')
       rescue Faraday::Error::ConnectionFailed
         redirect full_path('/followings')
@@ -228,41 +137,35 @@ module TentD
     end
 
     delete '/followings/:id' do
-      client = tent_client
-      client.following.delete(params[:id])
+      tent_client.following.delete(params[:id])
       redirect full_path('/followings')
     end
 
     get '/followers' do
-      client = tent_client
-      @followers = client.follower.list.body
+      @followers = tent_client.follower.list.body
       @followers.map! { |f| Hashie::Mash.new(f) }
       slim :followers
     end
 
     delete '/followers/:id' do
-      client = tent_client
-      client.follower.delete(params[:id])
+      tent_client.follower.delete(params[:id])
       redirect full_path('/followers')
     end
 
     get '/apps' do
-      client = tent_client
-      @apps = client.app.list.body
+      @apps = tent_client.app.list.body
       @apps.kind_of?(Array) ? @apps.map! { |a| Hashie::Mash.new(a) } : @apps = []
       @apps = @apps.sort_by { |a| -a.authorizations.size }
       slim :apps
     end
 
     delete '/apps/:app_id' do
-      client = tent_client
-      client.app.delete(params[:app_id])
+      tent_client.app.delete(params[:app_id])
       redirect full_path('/apps')
     end
 
     delete '/apps/:app_id/authorizations/:app_auth_id' do
-      client = tent_client
-      client.app.authorization.delete(params[:app_id], params[:app_auth_id])
+      tent_client.app.authorization.delete(params[:app_id], params[:app_auth_id])
       redirect full_path('/apps')
     end
 
@@ -274,8 +177,7 @@ module TentD
       session[:current_app_params] = @app_params
       @app_params = Hashie::Mash.new(@app_params)
 
-      client = tent_client
-      @app = session[:current_app] = client.app.get(@app_params.client_id).body
+      @app = session[:current_app] = tent_client.app.get(@app_params.client_id).body
       @app = @app.kind_of?(Hash) ? Hashie::Mash.new(@app) : @app
 
       @app ||= "Invalid client_id"
@@ -301,8 +203,7 @@ module TentD
         authorization = @app.authorizations.first
 
         unless authorization.notification_url == @app_params.tent_notification_url
-          client = tent_client
-          client.app.authorization.update(@app.id, authorization.id, :notification_url => @app_params.notification_url)
+          tent_client.app.authorization.update(@app.id, authorization.id, :notification_url => @app_params.notification_url)
         end
 
         redirect_uri.query +="&code=#{authorization.token_code}"
@@ -340,8 +241,7 @@ module TentD
         },
         :notification_url => @app_params.tent_notification_url
       }
-      client = tent_client
-      authorization = Hashie::Mash.new(client.app.authorization.create(@app.id, data).body)
+      authorization = Hashie::Mash.new(tent_client.app.authorization.create(@app.id, data).body)
 
       redirect_uri.query +="&code=#{authorization.token_code}"
       redirect_uri.query += "&state=#{@app_params.state}" if @app_params.has_key?(:state)
